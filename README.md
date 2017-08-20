0936ee35e3c1b45a59de799d360434988d1d120a
Eamonn O Nuallain <eamonn.o.nuallain@sap.com>
2017-08-20 20:32:53 +0100

    guess function added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # UNIX workbench - Eamonn O Nuallain
-Sun Aug 20 20:03:23 IST 2017
+Sun Aug 20 20:32:25 IST 2017
   
-3
+22
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-echo "hi"
+guessed=0
+actual=`wc -l < ./guessinggame.sh`
+
+function guess {
+  read guessed
+
+  while [[ "$guessed" -ne "$actual" ]]
+  do
+    if [[ "$guessed" -gt "$actual" ]]; then
+      echo "Sorry, too high, please guess again"
+    else
+      echo "Sorry, too low, please guess again"
+    fi
+    read guessed
+  done
+  echo "Correct! Thanks for playing"
+}
+
+echo "Guess how many lines are in the file guessinggame.sh"
+guess
